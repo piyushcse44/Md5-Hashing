dbe23935955cda07f0eb2a44c2b3b3d7f56fdc8a
piyush Kumar <piyushkumarcse44@gmail.com>
2023-12-02 02:26:21 +0530

    added css

--- Hashing/views.py
+++ Hashing/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from .hashing_fn import vigenere_encrypt
+from .hashing_fn import vigenere_encrypt,vigenere_decrypt
 
 def home(request):
     text = request.POST.get('PlainText')
     key = request.POST.get('key')
     message = vigenere_encrypt(str(text),str(key))
-    return render(request,'home.html',{'message' : message})
+    encypted_message=""
+    encypted_message = request.POST.get('EncyptMessage')
+    try:
+        decypted_message = vigenere_decrypt(str(encypted_message))
+    
+    except:
+        decypted_message = ""
+
+    
+    data ={
+        'PlainText':text,
+        'key':key,
+        'message' : message,
+        'flag': True,
+        'decypt_message': decypted_message,
+        'EncyptMessage':encypted_message,
+
+    }
+    return render(request,'home.html',data)
 
